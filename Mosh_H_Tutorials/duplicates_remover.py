65c22f1cb07d458647924b1767b8d0fe61db7bfb
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-05-11 14:51:38 +0100

    removes duplicate items

--- Mosh_H_Tutorials/duplicates_remover.py
+++ Mosh_H_Tutorials/duplicates_remover.py
@@ -1,6 +1,11 @@
 list = [5, 1, 3, 6, 8, 9, 1, 5, 7, 2, 2, 4]
+list2 = ["James", "John", "Orange", "SweetPotatoes", "Orange", "Peter", "John"]
 uniques = []
 for num in list:
     if num not in uniques:
         uniques.append(num)
+#print(uniques)
+for item in list2:
+    if item not in uniques:
+        uniques.append(item)
 print(uniques)
