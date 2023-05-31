73d88d0ed6ebd30f63dcf0a7b5a461a28da5a6a8
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-05-31 14:23:11 +0100

    I can use an else statement in a while loop

--- Jenny_K_Tutorials/while_loops.py
+++ Jenny_K_Tutorials/while_loops.py
@@ -1,4 +1,4 @@
-# A simple count statement in while else block
+"""# A simple count statement in while else block
 count = 1
 while count <= 5:
     print(count)
@@ -13,9 +13,9 @@ while count <= 5:
     print(count)
     count += 1
     if count == 3:
-        break
+        break  # Because this statement forcefully terminates the loop, the next block will not be printed
 else:
-    print("in else block")
+    print("in else block")  # This statement will be passed
 print("Out from loop")
 
 # An assignment
@@ -26,3 +26,21 @@ while count < 1:
 else:  # since the condition is false, it comes into the else block and out
     print("in else block")
 print("Out from loop")
+
+# Sentinel values
+number = int(input("enter a number (0  to quit): "))
+while number != 0:
+    print(number)
+    number = int(input("enter a number (0  to quit): "))
+else:
+    print("in else block")
+print("Out of else block")"""
+
+# Another assignment
+count = 0
+num = int(input("Enter a number(0 to quit): "))
+while num > 0:
+    print(num)
+    count += num
+    num = int(input("Enter a number(0 to quit): "))
+print(f"Total count is {count}")
